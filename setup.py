6f34ca75139f669b141b87f992a586b2701f3fee
zhanghongji <1029550448@qq.com>
2021-09-08 15:31:01 +0800

    0.2a1

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'qwebsite = qwebsite.__main__:cli',
+            'qwebsite = qwebsite.command:cli',
         ]}
 )
